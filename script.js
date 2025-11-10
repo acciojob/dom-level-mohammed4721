e8532e8fc155780084517a19e6e6018619dc0649
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-11-10 23:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let level = document.getElementById("level");
+
+let count = 0;
+let currentLevel = level;
+
+while (currentLevel != null) {
+	currentLevel = currentLevel.parentNode;
+	count++
+}
+
+alert("The level of the element is: " + count);
